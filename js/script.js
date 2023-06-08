59873fa080d638deb5201c6dd5152eb5715a2eb2
Sara Gabriela <saragabriela237@gmail.com>
2023-06-07 22:15:40 -0300

    committ inicial projeto portifólio

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function facebook(){
+    window.open("https://www.facebook.com/sara.gabriela.908?mibextid=LQQJ4d", "_blank");
+}
+
+function instagram(){
+    window.open("https://instagram.com/03sara_gabriela?igshid=NTc4MTIwNjQ2YQ==", "_blank");
+}
+
+function linkedin(){
+    window.open("https://www.linkedin.com/in/sara-gabriela-moura-rom%C3%A3o-4857b9230", "_blank");
+}
+
+function github(){
+    window.open("https://github.com/SaraGabrielaMR", "_blank");
+}
+
+function css3(){
+    window.open("https://saragabrielamr.github.io/calculadora-imc-css-js/", "_blank");
+}
+
+function javascript(){
+    window.open("https://saragabrielamr.github.io/conversao-de-unidades-de-medidas-js/", "_blank");
+}
+
+function html5(){
+    window.open("https://saragabrielamr.github.io/pagina-basica-html/", "_blank");
+}
+
+function codigo(){
+    window.open("https://github.com/SaraGabrielaMR/calculadora-imc-css-js", "_blank");
+}
